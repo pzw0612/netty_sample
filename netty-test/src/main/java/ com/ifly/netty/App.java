b3fabe5a39687af7761c6e152b1afeaa723746fa
庞志旺 <jonas.pang@viewfin.com>
2018-11-29 17:17:30 +0800

    the first commit project

---
+++ netty-test/src/main/java/ com/ifly/netty/App.java
@@ -0,0 +1,17 @@
+package  com.ifly.netty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        for(int i=0; i<100; i++){
+            System.out.println(i);
+        }
+    }
+}
